61092d1640929095d66ac9824d768ee11b059e04
ltrnd <67213735+ltrnd@users.noreply.github.com>
2025-04-13 17:47:37 -0300

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-    from flask import Flask, request, jsonify
-    from flask_cors import CORS
-    
-    app = Flask(__name__)
-    CORS(app)
-    
-    # Localização armazenada em memória (simples)
-    localizacao = {"latitude": None, "longitude": None, "timestamp": None}
-    
-    @app.route("/enviar", methods=["POST"])
-    def receber_localizacao():
-     data = request.get_json()
-     localizacao["latitude"] = data.get("latitude")
-     localizacao["longitude"] = data.get("longitude")
-     localizacao["timestamp"] = data.get("timestamp")
-     return jsonify({"status": "ok", "mensagem": "Localização recebida com sucesso!"})
- 
-    @app.route("/localizacao", methods=["GET"])
-    def enviar_localizacao():
-     return jsonify(localizacao)
- 
-    if __name__ == "__main__":
-      app.run()
-  
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Localização armazenada em memória (simples)
+localizacao = {"latitude": None, "longitude": None, "timestamp": None}
+
+@app.route("/enviar", methods=["POST"])
+def receber_localizacao():
+    data = request.get_json()
+    localizacao["latitude"] = data.get("latitude")
+    localizacao["longitude"] = data.get("longitude")
+    localizacao["timestamp"] = data.get("timestamp")
+    return jsonify({"status": "ok", "mensagem": "Localização recebida com sucesso!"})
+
+@app.route("/localizacao", methods=["GET"])
+def enviar_localizacao():
+    return jsonify(localizacao)
+
+if __name__ == "__main__":
+    app.run()
